81bbd47e70e59a3333e58b555a0fae3718d9d9b3
Patrick Zenhäusern <patrick.zenhaeusern@digitecgalaxus.ch>
2023-07-08 15:42:49 +0200

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { FC, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FC, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 export const App: FC = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,5 +29,5 @@ export const App: FC = () => {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
